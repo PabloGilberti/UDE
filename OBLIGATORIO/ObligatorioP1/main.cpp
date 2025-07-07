18624424e35d775a49cbe55a53e224346043cb30
NBBigua <pgilberti@gmail.com>
2025-07-07 16:10:06 -0300

    Verificaciones de Ingreso de Datos

--- OBLIGATORIO/ObligatorioP1/main.cpp
+++ OBLIGATORIO/ObligatorioP1/main.cpp
@@ -9,6 +9,7 @@ typedef  char superHeroes[MAX_HEROES];
 typedef  char superPoderes[MAX_PODERES];
 typedef  char String[MAX];
 typedef enum {FALSE,TRUE} boolean;
+typedef int Matriz[MAX_HEROES][MAX_PODERES];
 
 int main()
 {
@@ -17,9 +18,15 @@ int main()
     String heroe,nombre,universo,poder;
     char letra;
     int i=0,j=0,k=0;
-    superHeroes nombreHeroes[MAX_HEROES];
+    superHeroes nombreHeroes[MAX];
+    superPoderes descPoderes[MAX];
     boolean esMarvel, esDC;
-//CARGAR JUGADOR
+    Matriz opinionUser;
+
+/************************************************************************************
+**************************  CARGAR NOMBRE JUGADOR  **********************************
+************************************************************************************/
+
     printf("\nIngrese su nombre: ");
     fflush(stdin);
     scanf ("\n%c", &letra);
@@ -30,7 +37,9 @@ int main()
     }
     nombre[i]='\0';
 
-//CARGAR EL UNIVERSO
+/************************************************************************************
+****************************  CARGAR UNIVERSO  **************************************
+************************************************************************************/
     i=0;
     printf("\nIngrese el universo MARVEL o DC: ");
     fflush(stdin);
@@ -72,40 +81,120 @@ int main()
         return 0;
     }
 
-//CARGAR EL SUPER HEROE
-
-
+/************************************************************************************
+**************************  CARGAR SUPER HEROE  *************************************
+************************************************************************************/
 
     for (i=0;i<MAX_HEROES;i++){
         printf("Ingrese nombre del superhéroe %d (ENTER para finalizar): ", i + 1);
         j=0;
+        fflush(stdin);
         do{
-            scanf ("%c", &letra);
+
+            scanf ("%c",&letra);
             if(letra != '\n' ){
                nombreHeroes[i][j]=letra;
-               j++;
-            }
+               j++;            }
+
         }while (letra != '\n' && j < MAX-1);
         nombreHeroes[i][j]='\0';
-        //k=0;
-        //while (heroe[k] != '\0') {
-          //  nombreHeroes[j][k] = heroe[k];
-            //k++;
-        //}
-       // nombreHeroes[j][k] = '\0';
 
+    }
+     printf("Superhéroes:\n");
+    for (i = 0; i < MAX_HEROES; i++) {
+        fflush(stdin);
+        printf("[%d]", i);
+        j = 0;
+        while (nombreHeroes[i][j] != '\0') {
+            fflush(stdin);
+            printf("%c", nombreHeroes[i][j]);
+            j++;
+        }
+    printf("\n");
     }
 
 
 
+/************************************************************************************
+**************************  CARGAR SUPER PODER  *************************************
+************************************************************************************/
 
-//IMPRIMIR LO CARGADO
+    for (i=0;i<MAX_PODERES;i++){
+        printf("Ingrese 4 Super poderes d (ENTER para finalizar): ", i + 1);
+        j=0;
+        fflush(stdin);
+        do{
+            scanf ("%c", &letra);
+            if(letra != '\n' ){
+               descPoderes[i][j]=letra;
+               j++;
+            }
+        }while (letra != '\n' && j < MAX-1);
+        descPoderes[i][j]='\0';
+    }
+       printf("Super Poderes:\n");
+
+    for (i = 0; i < MAX_PODERES; i++) {
+        printf("  [%d]", i);
+        j = 0;
+        fflush(stdin);
+        while (descPoderes[i][j] != '\0') {
+            printf("%c", descPoderes[i][j]);
+            j++;
+        }
+    printf("\n");
+    }
+/************************************************************************************
+******************************  CARGAR LA JUGADA  ***********************************
+************************************************************************************/
+
+
+  for (i = 0; i < MAX_HEROES; i++) {
+    for (j = 0; j < MAX_PODERES; j++) {
+        printf("Ingrese la puntuación del superhéroe: ");
+
+        //mostrar el super heroe
+        k = 0;
+        fflush(stdin);
+        while (nombreHeroes[i][k] != '\0') {
+            printf("%c", nombreHeroes[i][k]);
+            k++;
+        }
+
+        printf(" con el poder ");
+
+        // mostrar el superpoder
+        k = 0;
+        fflush(stdin);
+        while (descPoderes[j][k] != '\0') {
+            printf("%c", descPoderes[j][k]);
+            k++;
+        }
+
+        printf(" (0 a 5): ");
+        scanf("%d", &opinionUser[i][j]);
+
+        // valido la opinion del user de 0 a 5
+        while (opinionUser[i][j] < 0 || opinionUser[i][j] > 5) {
+            printf("Valor inválido. Debe estar entre 0 y 5. Intente nuevamente: ");
+            scanf("%d", &opinionUser[i][j]);
+        }
+    }
+}
+
+/************************************************************************************
+**********************  VERIFICACIONES DE LO CARGADO  *******************************
+************************************************************************************/
+
+//USUARIO
     i = 0;
     printf ("\nEl Jugador ingresado es: ");
     while (nombre[i] != '\0') {
            printf ("%c", nombre[i]);
            i++;
     }
+
+//UNIVERSO
     i = 0;
     printf ("\nEl Universo ingresado es: ");
     while (universo[i] != '\0') {
@@ -113,41 +202,29 @@ int main()
            i++;
     }
 
-  printf("\nSuperhéroes:\n");
-for (i = 0; i < MAX_HEROES; i++) {
-    printf("  [%d] ", i);
-    j = 0;
-    while (nombreHeroes[i][j] != '\0') {
-        printf("%c", nombreHeroes[i][j]);
-        j++;
-    }
+
+//SUPERHEROE
+    printf("\nSuperhéroes:\n");
+    for (i = 0; i < MAX_HEROES; i++) {
+        printf("  [%d] ", i);
+        j = 0;
+        while (nombreHeroes[i][j] != '\0') {
+            printf("%c", nombreHeroes[i][j]);
+            j++;
+        }
     printf("\n");
-}
-
-    /*scanf("\n%c",&nombre);
-
-    printf("\nIngrese Universo MARVEL o DC: ");
-    fflush(stdin);
-    scanf("\n%c",&universo);
-
-    printf("\nIngrese 5 Superheroes del universo elegido");
-
-    for (i=0;i<MAX_HEROES;i++){
-            printf("\nHeroe Numero:%d",i+1);
-            fflush(stdin);
-            scanf("%c",&nombreHeroe[i]);
-
-
     }
 
-    for (i=0;i<MAX_HEROES;i++){
-            printf("\nUsted ingreso los siguientes Super Heroes:%c");
-            fflush(stdin);
-            scanf("%c",&nombreHeroe[i]);
-
-
+ //SUPER PODERES
+    printf("\nSuper Poderes:\n");
+    for (i = 0; i < MAX_PODERES; i++) {
+        printf("  [%d] ", i);
+        j = 0;
+        while (descPoderes[i][j] != '\0') {
+            printf("%c", descPoderes[i][j]);
+            j++;
+        }
+    printf("\n");
     }
 
-
-*/
 }
