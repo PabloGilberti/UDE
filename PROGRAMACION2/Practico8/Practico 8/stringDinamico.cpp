12ae1533a946514bfe987e9cbed4d6da3e6c5a4c
Pablo <pgilberti@gmail.com>
2025-10-16 13:45:44 -0300

    Practico 8

---
+++ PROGRAMACION2/Practico8/Practico 8/stringDinamico.cpp
@@ -0,0 +1,128 @@
+#include "stringDinamico.h"
+#include <stdio.h>
+
+
+
+
+
+// Devuelve longitud de la cadena
+int strlar(String s){
+
+    int i=0;
+    while(s[i]!='\0'){
+
+        i++;
+    }
+
+    return i;
+
+}
+
+
+// Imprime la cadena
+void print(String &s){
+
+    int i=0;
+    while(s[i]!='\0'){
+
+            printf("%c",s[i]);
+            i++;
+    }
+
+
+}
+
+// Carga la cadena por teclado
+void scan(String &s){
+
+
+
+
+
+
+
+}
+
+
+// Devuelve TRUE si s1 < s2
+boolean strmen(String s1, String s2);
+
+// Devuelve TRUE si s1 == s2
+boolean streq(String s1, String s2){
+    boolean iguales=TRUE;
+    int i = 0;
+
+    while (s1[i]!='\0' && s2[i]!='\0'){
+
+            if(s1[i]!=s2[i])
+                iguales=FALSE;
+            else
+                i++;
+
+    }
+    if (s1[i]=='\0' != s2[i]=='\0')
+            iguales=FALSE;
+
+    return iguales;
+}
+
+// Copia s2 en s1
+void strcop(String &s1, String s2){
+
+    delete[]s1;
+    int largo = strlar(s2);
+    s1=new char[largo + 1];
+    int i=0;
+    while (s2[i]!='\0'){
+
+
+        s1[i]=s2[i];
+        i++;
+    }
+    s1[i]='\0';
+}
+
+// Concatena s1 y s2
+void strcon(String &s1, String s2){
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+// Intercambia s1 y s2
+void strswp(String &s1, String &s2);
+
+//Crea un string vacío (solo con el carácter ‘\0’)
+void strcrear(String &s){
+
+
+ s = new char[1];
+ s [0]= '\0';
+
+
+
+}
+
+//Libera la memoria ocupada por el string y lo deja nulo
+void strDestruir(String &s){
+
+        delete[]s;
+        s=NULL;
+
+}
+
---
+++ PROGRAMACION2/Practico8/Practico 8/stringDinamico.h
@@ -0,0 +1,38 @@
+#ifndef STRINGDINAMICO_H_INCLUDED
+#define STRINGDINAMICO_H_INCLUDED
+#include "boolean.h"
+
+const int MAX = 80;
+typedef char * String;
+
+// Devuelve longitud de la cadena
+int strlar(String s);
+
+// Imprime la cadena
+void print(String &s);
+
+// Carga la cadena por teclado
+void scan(String &s);
+
+// Devuelve TRUE si s1 < s2
+boolean strmen(String s1, String s2);
+
+// Devuelve TRUE si s1 == s2
+boolean streq(String s1, String s2);
+
+// Copia s2 en s1
+void strcop(String &s1, String s2);
+
+// Concatena s1 y s2
+void strcon(String &s1, String s2);
+
+// Intercambia s1 y s2
+void strswp(String &s1, String &s2);
+
+//Crea un string vacío (solo con el carácter ‘\0’)
+void strcrear(String &s);
+
+//Libera la memoria ocupada por el string y lo deja nulo
+void strDestruir(String &s);
+
+#endif // STRINGDINAMICO_H_INCLUDED
