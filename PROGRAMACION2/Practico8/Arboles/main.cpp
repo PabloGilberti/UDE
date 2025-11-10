98881b3855c44a26d341ddf223568b9a055fac52
PabloGilberti <pgilberti@gmail.com>
2025-11-10 07:37:15 -0300

    Practico8

---
+++ PROGRAMACION2/Practico8/Arboles/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+typedef struct nodoa{ int info;
+                      nodoa * hizq;
+                      nodoa * hder;
+                    }nodo;
+typedef nodo * Arbol;
+
+
+
+
+
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
