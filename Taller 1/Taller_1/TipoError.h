7bc30c76a119ab5d9d1fd6afdd091e60b70aec47
Pablo <pgilberti@gmail.com>
2026-02-09 19:44:20 -0300

    Se generan los cpp del proyecto del taller1

--- Taller 1/Taller_1/TipoError.h
+++ Taller 1/Taller_1/TipoError.h
@@ -2,5 +2,14 @@
 #define TIPOERROR_H_INCLUDED
 
 
+typedef enum {
+                ERROR_NINGUNO,               // No hay error
+                ERROR_CARACTER_INVALIDO,     // Símbolo desconocido
+                ERROR_TERMINO_INVALIDO,      // Termino mal formado
+                ERROR_PARENTESIS,            // Paréntesis mal balanceados
+                ERROR_OPERADOR,              // Operador inválido o mal ubicado
+                ERROR_VARIABLE,              // Variable inválida
+                ERROR_EXPRESION_INCOMPLETA   // Falta operando u operador
+                } TipoError;
 
 #endif // TIPOERROR_H_INCLUDED
