2f10bc714d3b5ea9d92b96121246149a31fc6a93
Pablo <pgilberti@gmail.com>
2026-02-23 18:54:21 -0300

    Se agregan procedimientos finales a lista string

--- Taller 1/Taller_1/ListaString.cpp
+++ Taller 1/Taller_1/ListaString.cpp
@@ -1,5 +1,4 @@
 #include "ListaString.h"
-#include <cstring>
 
 
 void   LS_Crear(ListaString &L){
@@ -57,7 +56,20 @@ void   LS_AgregarFinal(ListaString &L, const char* s){ // copia dinámica
 
     // Reservar memoria para el string
     nuevo->palabra = new char[strlar(s) + 1];
+
+/*
+    strlar(s) devuelve el largo de la cadena (sin contar '\0').
+
+    + 1 reserva espacio para el '\0' final.
+
+    new char[...] crea un array dinámico de caracteres.
+
+    nuevo->palabra ahora apunta a un bloque de memoria propio donde se guardará la copia.
+
+*/
     strcop(nuevo->palabra, s);
+    //Copia el texto al nuevo nodo
+
     nuevo->sig = NULL;
 
     if (L == NULL)
@@ -76,6 +88,76 @@ void   LS_AgregarFinal(ListaString &L, const char* s){ // copia dinámica
 
 }
 
-void   LS_Split(const char* linea, ListaString& L);//Cortar un string
+void LS_Split(const char* linea, ListaString &L){//Cortar el string
+    // Inicializar lista vacía
+    L = NULL;
 
-void   LS_Liberar(ListaString& L);
+    int i = 0; //indice qu voy recorriendo
+    int largo = strlar(linea);//cantidad de carcteres
+
+    while (i < largo)
+    {
+        // 1) Saltear espacios
+        //Ejemplo si tenés " simple 5":
+        //avanza i hasta la primera letra de simple.
+        while (i < largo && linea[i] == ' ')
+        {
+            i++;
+        }
+
+        //Si llego al final, corto
+        if (i >= largo)
+            break;
+
+        //Empezar a armar la palabra
+        //Guardo el indice donde comienza la palabra
+        int inicio = i;
+
+        //Voy a ir avanzando mientras sea una palabra (NO ESPACIOS)
+        while (i < largo && linea[i] != ' ')
+        {
+            i++;
+        }
+
+        int fin = i;
+
+        //Calcular tamaño palabra
+        //Si la Palabra es "compuesta" inicio apunta a la C y Fin a la ultima letra (seria 9)
+        int tamano = fin - inicio;
+
+        //Crear un especie de  buffer temporal para copiar el split
+        char* palabra = new char[tamano + 1];
+        //copia caracter por caracter
+        for (int j = 0; j < tamano; j++)
+        {
+            palabra[j] = linea[inicio + j];
+        }
+        //finaliza el string
+        palabra[tamano] = '\0';
+
+        //Agregar a la lista
+        LS_AgregarFinal(L, palabra);
+
+        //Liberar buffer temporal
+        delete[] palabra;
+    }
+}
+
+void   LS_Liberar(ListaString &L)
+{
+
+    //No podemos mover directamente L porque perderíamos referencia.
+    NodoS * aux = L;
+
+        //voy a recorre toda la lista mintra tenga nodos
+    while (aux != NULL)
+    {
+        NodoS * borrar = aux;   // guardo nodo actual
+        aux = aux->sig;        // avanzo antes de borrar
+
+        delete[] borrar->palabra;  // liberar string
+        delete borrar;             // liberar nodo
+    }
+
+    L = NULL;  // dejar lista vacía
+}
